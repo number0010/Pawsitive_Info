dbfed9a4872173676461863478c4f5c49613d198
number0010 <number9294@naver.com>
2023-12-07 14:02:30 +0900

    모든페이지 적용 -로고 클릭시 index.html 이동 -메뉴바 변경사항 적용
    index.html 적용
    - 컨텐츠 클릭시(메뉴아님) 해당페이지로 이동
    - 배너 앞으로가기 뒤로가기 버튼 구현

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let currentSlide = 0;
 const slides = document.querySelectorAll('.slide');
 const slideCount = slides.length;
- 
+
+
+const bnrLeftBtn = document.querySelector("#bannerLeftBtn") 
+const bnrRightBtn = document.querySelector("#bannerRightBtn") 
+
 function showSlide(n) {
     slides.forEach(slide => slide.style.display = 'none');
     slides[n].style.display = 'block';
@@ -20,4 +24,8 @@ function prevSlide() {
 document.addEventListener('DOMContentLoaded', () => {
     showSlide(currentSlide);
     setInterval(nextSlide, 5000); // 3초마다 자동 슬라이드                
+    bnrLeftBtn.addEventListener('click',prevSlide)
+    bnrRightBtn.addEventListener('click',nextSlide)
+
 });
+
